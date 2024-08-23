d44ae810991b2f74ea35163c0b8994f7d7a3378c
George Bertios <bertiosgiorgos@yahoo.com>
2024-08-23 14:15:32 +0300

    Code untill 'Build a Screen step 6'

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import  ImageViewer  from './components/ImageViewer';
+import Button from './components/Button';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo"/>
+        <Button label="Use this photo"/>
+      </View>
     </View>
   );
 }
@@ -13,8 +22,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
+    alignItems: 'center'
+  },
+  imageContainer: {
+    flex:1,
+    paddingTop:58,
+  },
+  footerContainer:{
+    flex: 1 / 3,
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
