d1a02dfbd0bdda5c86608e20159d185a50b2c195
George Bertios <bertiosgiorgos@yahoo.com>
2024-08-28 21:48:08 +0300

    Until the completion of section 'Configure status bar, splash screen and app icon'

--- App.js
+++ App.js
@@ -121,7 +121,7 @@ export default function App() {
             <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
          </EmojiPicker>
 
-         <StatusBar style="auto" />
+         <StatusBar style="light" />
      
     </GestureHandlerRootView>
   );
